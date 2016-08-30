b625dd327a584acac5c83f63e662b884119a5e39
Onur Küçük <onur@delipenguen.net>
2016-08-30 18:25:13 +0300

    change route code style

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users, :path_names => {
-    sign_in: 'login',
-    sign_out: 'logout',
-    sign_up: 'register'
+    :sign_in => 'login',
+    :sign_out => 'logout',
+    :sign_up => 'register'
   }
 
   root :to => 'home#index'
